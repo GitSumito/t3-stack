b11cc8c8d471ea6e655f2306752f390919003c70
sumito tsukada <tsukada@sumito.jp>
2023-05-02 23:17:47 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,30 @@
+import { type NextPage } from "next";
+import { Auth } from "../components/Auth";
+import { Layout } from "../components/Layout";
+import Link from "next/link";
+import { signOut, useSession } from "next-auth/react";
+import { ArrowLeftOnRectangleIcon} from "@heroicons/react/24/solid";
+
+const Home: NextPage = () => {
+  const { data: session } = useSession();
+  if (!session){
+    return (
+      <Layout title="Login">
+        <Auth />
+      </Layout>
+    )
+  }
+
+  return (
+    <Layout title="Todo App">
+      <ArrowLeftOnRectangleIcon
+        className="h-6 w-6 cursor-pointer text-blue-600"
+        onClick={() => signOut()} />
+      <p className="my-3 text-xl text-blue-600">
+        {session?.user?.name}</p>
+    </Layout>
+  );
+};
+
+export default Home;
+
